eed893c9316b8f7daf7381e97be2c5f44de3de99
shipulinia <shipulinia@lad24.ru>
2019-09-23 15:36:20 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const strArr = str.split("");
+    const lastOpened = {
+        config: null,
+        index: null
+    };
+    let j = 0;
+    while (0 < strArr.length && j < strArr.length) {
+        let index = null;
+        bracketsConfig.forEach((item, i) => {
+            if (item.includes(strArr[j])) {
+                index = i;
+            }
+        });
+        if (bracketsConfig[index][0] === bracketsConfig[index][1]) {
+            if (
+                j - 1 === lastOpened["index"] &&
+                lastOpened["config"] === index
+            ) {
+                strArr.splice(j - 1, 2);
+                j = 0;
+                continue;
+            }
+        }
+        if (strArr[j] === bracketsConfig[index][0]) {
+            lastOpened["config"] = index;
+            lastOpened["index"] = j;
+        } else if (strArr[j] === bracketsConfig[index][1]) {
+            if (
+                j - 1 === lastOpened["index"] &&
+                lastOpened["config"] === index
+            ) {
+                strArr.splice(j - 1, 2);
+                j = 0;
+                continue;
+            } else {
+                return false;
+            }
+        }
+        j++;
+    }
+    return strArr.length === 0;
+};
